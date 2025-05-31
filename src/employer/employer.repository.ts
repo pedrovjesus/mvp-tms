dc20c904301548bfbcdd49a0674632d0da6b2ea0
pedrovjesus <pedroferreira161jesus@gmail.com>
2025-05-31 17:04:43 -0300

    fix(employer): correct repo name

--- src/employer/employer.repository.ts
+++ src/employer/employer.repository.ts
@@ -9,16 +9,16 @@ import { updateEmployerDto } from './dto/update-employer.dto';
 export class EmployerRepository {
   constructor(
     @InjectRepository(Employer)
-    private readonly employerRepository: Repository<Employer>,
+    private readonly repo: Repository<Employer>,
   ) {}
 
   async createEmployer(dto: createEmployerDto): Promise<Employer> {
-    const employer = this.employerRepository.create(dto);
-    return this.employerRepository.save(employer);
+    const employer = this.repo.create(dto);
+    return this.repo.save(employer);
   }
 
   async getAllEmployers(): Promise<Employer[]> {
-    return await this.employerRepository.find();
+    return await this.repo.find();
   }
   async getOneEmployer(filter: {
     id?: number;
@@ -26,39 +26,39 @@ export class EmployerRepository {
     name?: string;
   }): Promise<Employer> {
     if (filter.id) {
-      return await this.employerRepository.findOneBy({ id: filter.id });
+      return await this.repo.findOneBy({ id: filter.id });
     }
 
     if (filter.cpf) {
-      return await this.employerRepository.findOneBy({ cpf: filter.cpf });
+      return await this.repo.findOneBy({ cpf: filter.cpf });
     }
 
     if (filter.name) {
-      return await this.employerRepository.findOneBy({ name: filter.name });
+      return await this.repo.findOneBy({ name: filter.name });
     }
 
     return null;
   }
 
   async getEmployerLikeName(name: string): Promise<Employer[]> {
-    return await this.employerRepository.find({
+    return await this.repo.find({
       where: { name: Like(`%${name}%`) },
     });
   }
 
   async deleteEmployer(filter: { id?: number; cpf?: string }): Promise<void> {
     if (filter.id) {
-      await this.employerRepository.delete(filter.id);
+      await this.repo.delete(filter.id);
       return;
     }
     if (filter.cpf) {
-      await this.employerRepository.delete({ cpf: filter.cpf });
+      await this.repo.delete({ cpf: filter.cpf });
       return;
     }
   }
 
   async updateEmployer(id: number, dto: updateEmployerDto): Promise<Employer> {
-    await this.employerRepository.update(id, dto);
-    return this.employerRepository.findOneBy({ id });
+    await this.repo.update(id, dto);
+    return this.repo.findOneBy({ id });
   }
 }
